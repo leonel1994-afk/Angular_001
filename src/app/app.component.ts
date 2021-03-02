b26f5ee814bcefca0b8d616c50f6219583311928
rchagmana5056 <rchagmana5056@uta.edu.ec>
2021-03-02 17:13:39 -0500

    commit respaldo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular001';
+  diaSemana: string ='Martes_2_de_marzo_2021';
+  nombre = 'Luis';
+  apellido='Torres';
+
+  cadenaTexto: string='Su nombre';
+ sumar(x:number, y:number){
+   return x + y; 
+ }
+
+ mostrarMensaje(){
+   return 'Cadena de texto';
+}
+
 }
